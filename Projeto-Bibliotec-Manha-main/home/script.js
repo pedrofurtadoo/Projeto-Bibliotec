11f0762ace67fa7897d190e974c1ceecdc1fa1a8
PEDRO STAIN FURTADO <46161394820@info.local>
2026-09-11 11:01:11 -0300

    :sparkles: introdução java script

---
+++ Projeto-Bibliotec-Manha-main/home/script.js
@@ -0,0 +1,45 @@
+
+let nome = "Felipe";
+let idade = 25;
+
+//CONCATENAÇÃO
+console.log("Nome: " + nome + ", idade: " + idade);
+
+//INTERPOLAÇÃO
+console.log(`Nome: ${nome}, idade: ${idade}`)
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+let soma = "5" + 3
+let sub = "5"
+
+//OPERADORES RELACIONAIS 
+
+//VALIDA IGUALDADE DE VALOR
+
+//"10" == 10 -> true
+
+//VALIDADE IGUALDADE DE VALOR E TIPO DE DADO
+//"10" === 10 -> false
+
+//&&
+//||
+// !
+
+//let opcao = 2
+
+//switch(opcao) {
+//     case 1: dhaSAsoha
+//            break;
+//
+//}
+
+//ESTRUTURA DE REPETIÇÃO
+let frutas = ["Maçã", "Banana", "Acerola", "Uva"]
+
+console.log(frutas);
+
+frutas.forEach(function(pinto) {
+console.log("Nome: " + pinto)
+})
